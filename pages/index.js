01bfb02c0ec06b56c170137cd4e8cde2edcc724b
sulaiman282 <spidy.twitch.tv@gmail.com>
2023-05-13 20:48:27 +0600

    first

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Index() {
       <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img class="w-8 h-8 mr-2" src="/logo.png" className="object-contain" alt="logo"/>
+          <img  src="/logo.png" className="object-contain w-40 h-40" alt="logo"/>
          
       </a>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
