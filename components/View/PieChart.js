59784e603c2ac4b5cd51d2ee1bcc8be05ef1aae7
Mithun <mithunmani2597@gmail.com>
2023-06-26 23:24:29 +0530

    fix : Pie chart

--- components/View/PieChart.js
+++ components/View/PieChart.js
@@ -1,18 +1,34 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
-import { Pie } from "react-chartjs-2";
-import {Chart, ArcElement} from 'chart.js'
-Chart.register(ArcElement);
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-const data = {
-  type:'pie',
-  labels: ["In Stage", "Mapped", "Missing"],
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [
     {
-      data: [400, 50, 100],
-      backgroundColor: ["#9b2c2c", "#2f855a", "#434190"],
-      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
     },
   ],
 };
